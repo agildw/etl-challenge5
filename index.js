a957614cbb19396678e0f6d63d088dfa8bd1e444
agildw <yudistiraagil24@gmail.com>
2022-04-02 10:20:16 +0700

    add files

---
+++ index.js
@@ -0,0 +1,36 @@
+const { url } = require('inspector');
+const fetch = require('node-fetch');
+const readlineSync = require('readline-sync');
+
+
+const githubAccount = () => {
+    const getAccount = (username) => new Promise((resolve, reject) => {
+        fetch('https://api.github.com/users/' + username)
+            .then(result => result.json())
+            .then((result) => {
+                resolve(result)
+            })
+            .catch((error) => {
+                reject(error)
+            })
+    })
+
+    const sendResult = async () => {
+        console.log('Github user information')
+        const username = readlineSync.question('Masukkan username : ')
+        // console.log(username)
+        const dataInfo = await getAccount(username);
+        if (dataInfo['message']) {
+            console.log('username tidak ditemukan');
+        } else {
+            // console.log(dataInfo);
+            console.log('Hasil dari user ' + dataInfo['html_url']);
+            console.log('Nama : ' + dataInfo['name']);
+            console.log('Bio : ' + dataInfo['bio']);
+            console.log('Dibuat tanggal : ' + dataInfo['created_at'])
+        }
+
+    }
+    sendResult()
+}
+githubAccount();
